0dc81007e39def8e4955f38ceda98959f725055e
psnavya <69518202+psnavya@users.noreply.github.com>
2023-03-14 23:24:05 +0530

    Add files via upload

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -3,7 +3,8 @@
 describe("Test Contact Us form via WebdriverUni",() => {
     it("Should be able to submit a successful submission via contact us form", () => {
         //cypress code
-        
+        cy.visit("http://www.webdriveruniversity.com/")
+        cy.get('#contact-us > .thumbnail').click()
     });
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
