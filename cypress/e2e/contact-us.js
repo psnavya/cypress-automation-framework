437d2b7bb05ff2749115161bfe0cf8549c01a1f3
psnavya <69518202+psnavya@users.noreply.github.com>
2023-03-22 21:50:17 +0530

    Positive and Negetive scenario
    it.only to execute a particular test case

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -2,13 +2,24 @@
 
 describe("Test Contact Us form via WebdriverUni",() => {
     it("Should be able to submit a successful submission via contact us form", () => {
-        //cypress code
-        cy.visit("http://www.webdriveruniversity.com/")
-        cy.get('#contact-us').click({force:true})
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.get('#contact-us').click({force:true})
+        // cy.visit("https://www.amazon.in/your-account")
+        // cy.get('#twotabsearchtextbox').type("Tops for women")
+        // cy.get('#nav-search-submit-button').click()
+        cy.get('[name="first_name"]').type("Joe")
+        cy.get('[name="last_name"]').type("Bing")
+        cy.get('[name="email"]').type("joe.bing@gmail.com")
+        cy.get('textarea.feedback-input').type("Testing Cypress Automation")
+        cy.get('[type="submit"]').click()
     });
 
-    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        //cypress code
+    it.only("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Chandler")
+        cy.get('[name="last_name"]').type("Bing")
+        cy.get('textarea.feedback-input').type("Testing Cypress Automation")
+        cy.get('[type="submit"]').click()
     });
 })
 
