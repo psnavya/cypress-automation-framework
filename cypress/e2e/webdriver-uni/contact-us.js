5f9eb4695261fc991d7b90ea276e04d19bc94c7e
psnavya <69518202+psnavya@users.noreply.github.com>
2023-03-25 01:40:58 +0530

    Update contact-us.js

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,8 +1,7 @@
-
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni",() => {
-    it("Should be able to submit a successful submission via contact us form", () => {
+    it.only("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
         // cy.get('#contact-us').click({force:true})
         // cy.visit("https://www.amazon.in/your-account")
@@ -13,9 +12,10 @@ describe("Test Contact Us form via WebdriverUni",() => {
         cy.get('[name="email"]').type("joe.bing@gmail.com")
         cy.get('textarea.feedback-input').type("Testing Cypress Automation")
         cy.get('[type="submit"]').click()
+        cy.get('h1').should('have.text','Thank You for your Message!')
     });
 
-    it.on("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
         cy.get('[name="first_name"]').type("Chandler")
         cy.get('[name="last_name"]').type("Bing")
