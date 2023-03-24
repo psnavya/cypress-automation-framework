ad845cf632b151b0c034a0d256163b552710961b
psnavya <69518202+psnavya@users.noreply.github.com>
2023-03-25 00:45:00 +0530

    Update contact-us.js

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -1,14 +1,21 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
+/// <reference types="@cypress/xpath" />
 
 describe("Test Contact Us form via Automation Test Store",() => {
     it("Should be able to submit a successful submission via contact us form", () => {
        cy.visit("https://www.automationteststore.com/")
-    //    cy.visit("https://www.automationteststore.com/index.php?rt=content/contact")
-    cy.get('.info_links_footer > :nth-child(5) > a').click()
+    
+    // cy.get('.info_links_footer > :nth-child(5) > a').click()
+    // cy.xpath("//a[contains(@href,'contact')]").click()
+    cy.get("a[href$='contact']").click()
+
     cy.get('#ContactUsFrm_first_name').type("Joe")
     cy.get('#ContactUsFrm_email').type("joe.bing@gmail.com")
     cy.get('#ContactUsFrm_enquiry').type("Is there Cash on Delivery?")
-    cy.get('.col-md-6 > .btn').click()
+    
+    // cy.get('.col-md-6 > .btn').click()
+    cy.get("button[title='Submit']").click()
+    
 
 
     });
