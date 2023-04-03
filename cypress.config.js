3924c79c44f5fe21b94a6a024ead301b6b816395
psnavya <psnavya90@gmail.com>
2023-04-03 20:42:08 +0530

    CypreNetworkRequest API scenario

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     specPattern:"cypress/e2e/**/*.{ts,tsx,js,jsx,feature}"
   },
 });
-
+////////////////////////////////////////
 // //local-ui-api-juiceShop
 // import { defineConfig } from "cypress";
 
@@ -20,7 +20,7 @@ module.exports = defineConfig({
 //     specPattern: "cypress/e2e/**/*.js"
 //   },
 // });
-
+///////////////////////////////////////
 // //local-ui-angularRealWorld
 // import { defineConfig } from "cypress";
 
@@ -32,4 +32,16 @@ module.exports = defineConfig({
 //     specPattern: "cypress/e2e/**/*.js"
 //   },
 // });
+///////////////////////////////////////
+//cypress-network-requests
+// const { defineConfig } = require("cypress");
 
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//     specPattern: "cypress/e2e/**/*.js"
+//   },
+// });
+///////////////////////////////////////
